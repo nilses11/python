9dd885dc6be339c507d9623252600a2f07c310f0
nilses11 <nilses11@github.com>
2016-06-13 15:03:26 +0200

    Videre utvidelse. lagt til mye bloat...

--- ms.py
+++ ms.py
@@ -1,22 +1,44 @@
+from random import randint
 board = []
+explored = []
+boardsize = 20
+numbombs = 40
 
-board.append("*        *")
-board.append("          ")
-board.append("          ")
-board.append("          ")
-board.append("   *   *  ")
-board.append("     ***  ")
-board.append("          ")
-board.append("          ")
-board.append("  *       ")
-board.append("*        *")
+for p in range(boardsize):
+	explored.append([False for i in range(boardsize)])
 
+def createRandomBoard():
+	for i in range(boardsize):
+		s = ""
+		for o in range(boardsize):
+			if randint(0,numbombs) < 1:
+				s+="*"
+			else:
+				s+=" "
+		board.append(s)
+
+
+def createBoardWithExactNumbombs():
+	b2 = [[" " for x in range(boardsize)] for y in 
+range(boardsize)]
+	for i in range(numbombs):
+		y = randint(0, boardsize-1)
+		x = randint(0, boardsize-1)
+		b2[x][y] = '*'
+	for i in b2:
+		board.append("".join(i))
+	return board
+	
+
+#createRandomBoard()
+board = createBoardWithExactNumbombs()
+assert len(board) == len(board[0])
 
 def printneighbors(board):
 	newboard = []
-	for line in range(len(board)):
+	for line in range(boardsize):
 		newline = ""
-		for char in range(len(board[line])):
+		for char in range(boardsize):
 			newline+=str(neighbors(board, line, char))
 		newboard.append(newline)
 	printSolution(newboard)
@@ -29,17 +51,17 @@ def neighbors(board, x, y):
 		if board[x-1][y]=="*": count+=1
 		if y > 0:
 			if board[x-1][y-1]=="*": count+=1
-		if y < len(board)-1:
+		if y < boardsize-1:
 			if board[x-1][y+1]=="*": count+=1
-	if x < len(board)-1:
+	if x < boardsize-1:
 		if board[x+1][y]=="*": count+=1
 		if y > 0:
 			if board[x+1][y-1]=="*": count+=1
-		if y < len(board)-1:
+		if y < boardsize-1:
 			if board[x+1][y+1]=="*": count+=1
 	if y > 0:
 		if board[x][y-1]=="*": count+=1
-	if y < len(board)-1:
+	if y < boardsize-1:
 		if board[x][y+1]=="*": count+=1
 
 	if board[x][y]=="*":
@@ -48,6 +70,39 @@ def neighbors(board, x, y):
 	return count
 
 def printSolution(board):
-	print '  ABCDEFGHIJ'
-	for i in range(len(board)):
-		print str(i), board[i]
+	print '  0123456789'
+	print ''
+	for i in range(boardsize):
+		print str(i%10), getVal(board[i], explored[i])
+
+def getVal(line, linexp):
+	s = ""
+	for c in range(boardsize):
+		if linexp[c]:
+			s+=line[c]
+		else:
+			s+=" "
+	return s
+
+def explore(x, y):
+	if x < 0:
+		return -1
+	if y < 0:
+		return -1
+	if x > boardsize-1:
+		return -1
+	if y > boardsize-1:
+		return -1
+
+	explored[x][y] = True
+	if board[x][y] == '*':
+		return -1
+	elif neighbors(board, x, y) == 0:
+		if x > 0 and not explored[x-1][y]:
+			explore(x-1,y)
+		if x < boardsize-1 and not explored[x+1][y]:
+			explore(x+1,y)
+		if y > 0 and not explored[x][y-1]:
+			explore(x,y-1)
+		if y < boardsize-1 and not explored[x][y+1]:
+			explore(x,y+1)
--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 import ms
 
+
+loop = 0
+
+
 ms.printneighbors(ms.board)
+
+while loop != -1:
+	x,y = input()
+	loop = ms.explore(y,x)
+	ms.printneighbors(ms.board)
+
