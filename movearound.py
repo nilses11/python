b97ae03257b629ae9c5dd821f98f74506167d7eb
nilses11 <nilses11@github.com>
2016-12-14 15:47:31 +0100

    removed file. Minor update

--- movearound.py
+++ movearound.py
@@ -1,76 +1,78 @@
-from curses import wrapper
+from curses import wrapper, error
 from curses import KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT
 import pdb
 
-y = 9
-x = 11
-loop = True
-dirt = []
-maxyx = []
-
+state = {
+	'player' : [9, 11],
+	'loop' : True,
+	#dirt : []
+}
 def main(win):
-	global maxyx
+	global state
 	win.box()
-	maxyx = win.getmaxyx()
+	state['maxyx'] = win.getmaxyx()
+	state['win'] = win
 	try:
 		win.curs_set(0)
 	except:
 		pass
 	#Set start position
-	win.addch(y, x, '&')
+	y, x = state['player']
+	#win.addch(y, x, '&')
 	win.move(y, x)
 	
-	while loop:
+	while state['loop']:
 		key = win.getkey()
-		win.addch(y, x, ' ')
 		if key in keymappings:
-			keymappings[key]()
-		checkForCollision(win, y, x)
-		drawChanges(win)
+			keymappings[key](state)
 
-def quit():
-	global loop
-	loop = False
+def debug(state):
+	pdb.set_trace()
 
-def moveup():
-	global y
-	y-=1
+def quit(state):
+	state['loop'] = False
 
-def moveleft():
-	global x
-	x-=1
+def moveup(state):
+	if (state['player'][0]) > 1:
+		clearyx(state)
+		state['player'][0]-=1
 
-def moveright():
-	global x
-	x+=1
+def moveleft(state):
+	if (state['player'][1]) > 1:
+		clearyx(state)
+		state['player'][1]-=1
 
-def movedown():
-	global y
-	y+=1
+def moveright(state):
+	if (state['player'][1]) < state['maxyx'][1]-1:
+		clearyx(state)
+		state['player'][1]+=1
 
-def generateDirt():
-	global dirt
-	for yes in range(maxyx[0]-15):
-		line = ''
-		for xes in range(maxyx[1]-2):
-			line+='#'
-		dirt.append(line)
+def movedown(state):
+	if (state['player'][0]) < state['maxyx'][0]-1:
+		clearyx(state)
+		state['player'][0]+=1
+		
 
-def drawChanges(win):
-	global dirt
-	for i in range(len(dirt)):
-		line = dirt[i]
-		win.addstr(i+10, 1, line)
-	dirt = []
-	win.addch(y, x, '&')
-	win.move(y, x)
+def drawChanges(state):
+	y, x = state['player']
+	state['win'].addch(y, x, '0')
+	state['win'].move(y, x)
+	state['win'].redrawwin()
 
-def checkForCollision(win, oldy, oldx):
-	global x,y
-	if(win.inch(y,x) != 32):
-		pdb.set_trace()
-		x = oldx
-		y = oldy
+def clearyx(state):
+	y, x = state['player']
+	state['win'].addch(y, x, ' ')
+	state['win'].move(y, x)
+
+def generateDirt(state):
+	maxy, maxx = state['maxyx']
+	for y in range(1, maxy-1):
+		for x in range(1, maxx-1):
+			try:
+				state['win'].addch(y,x, ord('a'))
+			except error as e:
+				pass
+				pdb.set_trace()
 
 
 keymappings = {
@@ -79,10 +81,11 @@ keymappings = {
 		'KEY_LEFT' : moveleft,
 		'KEY_RIGHT' : moveright,
 		'q' : quit,
-		'g' : generateDirt
+		'g' : generateDirt,
+		'a' : drawChanges,
+		's' : clearyx,
+		'd' : debug
 		}
-
-
 
 if __name__ == '__main__':
 	wrapper(main)
